543cad77537f7626bb54a5a7771c44edf1a3f2cb
neeraj <neerajp4321@gmail.com>
2022-09-30 11:28:08 +0530

    Feat:add 2 rows data-on tables elements,properties

--- Periodic Table Database/periodic_table.sql
+++ Periodic Table Database/periodic_table.sql
@@ -97,6 +97,8 @@ INSERT INTO public.elements VALUES (8, 'O', 'Oxygen');
 INSERT INTO public.elements VALUES (2, 'He', 'Helium');
 INSERT INTO public.elements VALUES (3, 'Li', 'Lithium');
 INSERT INTO public.elements VALUES (1000, 'Mt', 'moTanium');
+INSERT INTO public.elements VALUES (9, 'F', 'Fluorine');
+INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 
 
 --
@@ -112,6 +114,8 @@ INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007, -210.1, -195.8, 2);
 INSERT INTO public.properties VALUES (8, 'nonmetal', 15.999, -218, -183, 2);
 INSERT INTO public.properties VALUES (5, 'metalloid', 10.81, 2075, 4000, 3);
 INSERT INTO public.properties VALUES (1000, 'metalloid', 1, 10, 100, 3);
+INSERT INTO public.properties VALUES (9, 'nonmetal', 18.998, -220, -188.1, 2);
+INSERT INTO public.properties VALUES (10, 'nonmetal', 20.18, -248.6, -246.1, 2);
 
 
 --
