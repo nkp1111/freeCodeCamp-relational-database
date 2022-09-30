28f1cff5dd9d12d3285f2424c7470c92a21790dd
neeraj <neerajp4321@gmail.com>
2022-09-30 18:25:22 +0530

    Feat: query user info into variable

--- number_guessing_game/number_guess.sh
+++ number_guessing_game/number_guess.sh
@@ -8,14 +8,16 @@ echo "Enter your username:"
 read USERNAME
 
 USER_DATA=$($PSQL "SELECT * FROM number_guess WHERE username = '$USERNAME'")
-echo $USER_DATA 
 
 # check if user exist
-if [[ -z $USER_DATA ]]
+if [[ -z $USER_DATA  ]]
 then
   # if no user exist
   echo "Welcome, $USERNAME! It looks like this is your first time here."
 else
   # if user exist
-  echo "Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses."
+  echo $USER_DATA | while IFS="|" read ID USERNAME GAMES_PLAYED BEST_GAME
+  do
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
 fi
