f80b7381c4ccece55c25590bd5a4f62a2662a45c
neeraj <neerajp4321@gmail.com>
2022-09-30 12:40:34 +0530

    Feat:initial query for both condition

--- periodic_table/element.sh
+++ periodic_table/element.sh
@@ -7,6 +7,10 @@ echo "Please provide an element as an argument."
 if [[ $1 =~ ^[0-9]+$ ]]
 then
   # atomic number
+  ELEMENT_DATA=$($PSQL "select * from elements where atomic_number = $1")
+  echo $ELEMENT_DATA
 else
   # element name
+  ELEMENT_DATA=$($PSQL "select * from elements where name ilike '$1%' limit 1")
+  echo $ELEMENT_DATA
 fi
