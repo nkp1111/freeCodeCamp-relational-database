4a2c9da3da7942f6775f13e2abb101764d08c244
neeraj <neerajp4321@gmail.com>
2022-09-30 18:00:31 +0530

    Feat: add initial query statement

--- number_guessing_game/number_guess.sh
+++ number_guessing_game/number_guess.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-PSQL="psql -username=freecodecamp --dbname=number_guess -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 RANDOM_NUMBER=$RANDOM
 
 echo "Enter your username:"
 read USER_NAME
+
+VAL=$($PSQL "select * from number_guess")
+echo $VAL
