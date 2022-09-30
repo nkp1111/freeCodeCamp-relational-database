d241af5a3f02f80a52f0d8263df9c352f62792ce
neeraj <neerajp4321@gmail.com>
2022-09-30 18:10:27 +0530

    Feat: add condition for diff user

--- number_guessing_game/number_guess.sh
+++ number_guessing_game/number_guess.sh
@@ -5,7 +5,17 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 RANDOM_NUMBER=$RANDOM
 
 echo "Enter your username:"
-read USER_NAME
+read USERNAME
 
-VAL=$($PSQL "select * from number_guess")
-echo $VAL
+USER_DATA=$($PSQL "SELECT * FROM number_guess WHERE username = '$USERNAME'")
+echo $USER_DATA 
+
+# check if user exist
+if [[ -z USER_DATA ]]
+then
+  # if no user exist
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  # if user exist
+  echo "Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses."
+fi
